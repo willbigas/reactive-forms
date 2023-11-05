04c8b6617af36688b0338aabb9f950bc8f75907d
William Bigas Mauro <william.mauro@ibm.com>
2023-11-05 12:42:49 -0300

    Commit initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ReactiveFormsComponent} from "./reactive-forms/reactive-forms.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ReactiveFormsComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
